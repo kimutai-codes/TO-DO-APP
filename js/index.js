4b359414813e0fe6f58235c22bb6504ea09e37d0
Allan <allankimutaikoech@gmail.com>
2020-06-03 12:37:00 +0300

    done!

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ let removeSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><
 
 let completeSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>`;
 
-//let completeSVG = ('/images/check-mark-isolated-on-transparent-600w-654663922.jpg')
 
 //Grab the 'add' button
 let addButton = document.getElementById('add');
@@ -61,7 +60,7 @@ function addItemTodo(text) {
 
 	let complete = document.createElement('button');
 	complete.classList.add('complete');
-	complete.innerText = 'COMPLETED';
+	complete.innerHTML= completeSVG ;
 	complete.addEventListener('click', completedItem);
 
 	manip.appendChild(remove);
@@ -74,13 +73,6 @@ function addItemTodo(text) {
 		item.remove();
 	}
 
-	// function completedItem() {
-	// 	completed();
-	// }
-
-	// function completed() {
-	// 	item.remove();
-	// }
 
 	let done = document.getElementById('completed');
 
@@ -92,6 +84,7 @@ function addItemTodo(text) {
 
 	function completedItem() {
 		item.remove();
-		done.insertBefore(item, list.childNodes[0]);
+		done.appendChild(item);
+		//done.insertBefore(itemTwo,)
 	}
 }
